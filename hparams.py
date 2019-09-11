7ed9b46985db7a000116570a19f1c933be9da561
linlinsongyun <37888350+linlinsongyun@users.noreply.github.com>
2019-09-11 14:41:54 +0800

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -8,11 +8,13 @@ hparams = tf.contrib.training.HParams(
   cleaners='english_cleaners',
 
   # Audio:
+  num+ppgs=252
+  num_lpc=32
   num_mels=80,
   num_freq=1025,
   sample_rate=16000,
-  frame_length_ms=50,
-  frame_shift_ms=12.5,
+  frame_length_ms=25,
+  frame_shift_ms=10,
   preemphasis=0.97,
   min_level_db=-100,
   ref_level_db=20,
